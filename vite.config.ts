33b2e592297065f93def98927932af7346938609
LiJun <lijunal@126.com>
2023-05-26 17:58:51 +0800

    调整vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/main.ts"),
+			name: "aegis-odyssey",
+			fileName: (format) => `aegis-odyssy.${format}.js`,
+		},
+		rollupOptions: {
+			// 把不想打包进你的包的包排除掉
+			external: ["vue"],
+			output: {
+				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+});
